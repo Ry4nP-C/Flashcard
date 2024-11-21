64d39569066676e4d45f4c78c1d88b65bfeac250
Ry4nP-C <00001118174550sp@al.educacao.sp.gov.br>
2024-11-21 10:29:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quadrinhos',
+    'Quem são os criadores do Homem-Aranha?',
+    'São Stan Lee e Steve Ditko'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a causa da entrada do Brasil na 2 Guerra Mundial?',
+    'A Causa foi que os navios mercantes foram torpedeados pela Alemanha Fazendo o Brasil se juntar aos Aliados'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual foi o jogo Mais vendido do PSP?',
+    'O Jogo mais vendido foi Grand Theft Auto:Liberty City Stories com 8 Milhões de Cópias Vendidas Até Março de 2008'
+)
